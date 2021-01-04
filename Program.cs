2d170d6b1c0d4c56b76afebdb007c3309f7e2c54
serapinta <serapinta90@hotmail.com>
2021-01-04 20:48:17 +0000

    Add Menu

--- Program.cs
+++ Program.cs
@@ -8,57 +8,6 @@ namespace MoonBuggy_2020
     {
         static void Main(string[] args)
         {
-            //ENTRADA
-           /* Console.WriteLine("  W       W  EEEEEEE L        CCCCC     OOOOO   MM     MM  EEEEEEE");
-            Console.WriteLine("  W   W   W  E       L       C         O     O  M M   M M  E ");
-            Console.WriteLine("  W  W W  W  EEEEE   L      C         O       O M  M M  M  EEEEE");
-            Console.WriteLine("  W W   W W  E       L       C         O     O  M   M   M  E");
-            Console.WriteLine("  WW     WW  EEEEEEE LLLLLLL  CCCCC     OOOOO   M       M  EEEEEEE");
-            Console.WriteLine();
-            Console.WriteLine("      TTTTTTT    OOOOO");
-            Console.WriteLine("         T      O     O");
-            Console.WriteLine("         T     O       O");
-            Console.WriteLine("         T      O     O");
-            Console.WriteLine("         T       OOOOO");
-            Console.WriteLine();
-            Console.WriteLine("              MM     MM    OOOOO     OOOOO    NN     N");
-            Console.WriteLine("              M M   M M   O     O   O     O   N N    N");
-            Console.WriteLine("              M  M M  M  O       O O       O  N  N   N");
-            Console.WriteLine("              M   M   M   O     O   O     O   N    N N");
-            Console.WriteLine("              M       M    OOOOO     OOOOO    N     NN");
-            Console.WriteLine();
-            Console.WriteLine("                  BBBBBBBB   U     U    GGGGG    GGGGG  Y      Y");
-            Console.WriteLine("                    B     B  U     U   G     G  G     G  Y    Y");
-            Console.WriteLine("                    BBBBBB   U     U  G        G          Y Y");
-            Console.WriteLine("                    B     B  U     U   G   GGG  G   GGG     Y");
-            Console.WriteLine("                    BBBBBB    UUUUU     GGGGG    GGGGG   YY");
-            Console.WriteLine();
-            //Console.ReadKey();
-            //Console.Clear();
-            //---------
-            //MENU
-            Console.WriteLine(" ==== MENU ==== ");
-            Console.WriteLine("  START");
-            Console.WriteLine("  INSTRUCTION");
-            Console.WriteLine("  CREDITS");
-            Console.WriteLine("  EXIT");
-            Console.WriteLine();
-            //Console.WriteLine(" ════════ ");
-            // ╔ ═ ═ ╗
-            // ╚  ═  ╝
-            //----------
-            //CARRO ANDAR
-            Console.WriteLine("  .-`---`-_   - - - ♦");
-            Console.WriteLine("  '=0===0=-'");
-            //CARRO PARADO
-            Console.WriteLine("  .-`---`-_   - - -");
-            Console.WriteLine("  '=O===O=-'");
-            //CHAO / SPIKES
-            Console.WriteLine(" ========▲▲▲===");
-            Console.WriteLine(" ==============");
-            //INIMIGO alt + pad4 ♦
-            Console.WriteLine("♦"); */
-
         }
     }
 }
